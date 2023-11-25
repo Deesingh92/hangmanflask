15f92d0176e71c2b8ab231ff9aac00031c548368
Dee Torna <tornadavinder@gmail.com>
2023-11-25 13:00:50 +0000

    database created in to the app

---
+++ hangman/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(50), nullable=False)
+    guessed_letters = db.Column(db.String(26), default="")
+    attempts = db.Column(db.Integer, default=0)
+
+class HangmanWord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(50), nullable=False)
