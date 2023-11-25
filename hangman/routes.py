a0d555d12fbdf229293fd373f5601bdf61c5f4ee
Dee Torna <tornadavinder@gmail.com>
2023-11-25 16:05:59 +0000

    hangman images added

--- hangman/routes.py
+++ hangman/routes.py
@@ -1,7 +1,6 @@
-from flask import render_template, request, redirect, url_for, flash
-from .models import db, Game, HangmanWord
+from flask import render_template, request, redirect, url_for
 from sqlalchemy import func
-import random
+from .models import db, Game, HangmanWord
 
 def get_random_word():
     word_record = HangmanWord.query.order_by(func.random()).first()
@@ -25,16 +24,11 @@ def configure_routes(app):
         game = Game.query.get_or_404(game_id)
 
         if request.method == 'POST':
-            letter = request.form['letter'].lower()  # Convert to lowercase for case-insensitivity
-            if letter.isalpha() and len(letter) == 1:  # Check if the input is a valid letter
-                if letter not in game.guessed_letters:
-                    game.guessed_letters += letter
-                    if letter not in game.word:
-                        game.attempts += 1
-                    db.session.commit()
-                else:
-                    flash("You already guessed that letter. Try another one.", 'warning')
-            else:
-                flash("Please enter a valid single letter.", 'danger')
+            letter = request.form['letter']
+            game.guessed_letters += letter
+            if letter not in game.word:
+                game.attempts += 1
+            db.session.commit()
 
-        return render_template('play.html', game=game)
+        min_attempts = min(game.attempts, 6)  # Adjust the maximum number of attempts as needed
+        return render_template('play.html', game=game, min_attempts=min_attempts)
